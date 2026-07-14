6cc891894bafd2d71ac83c62946f9a2bff1e4d74
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2026-07-13 21:55:16 -0300

    added deals store to module

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { authReducer } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { authReducer } from './reducers/auth.reducer';
+import { dealsReducer } from './reducers/deals.reducer';
+import { DealsEffects } from './effects/deals.effects';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot({ auth: authReducer }),
+    StoreModule.forRoot({ auth: authReducer, deals: dealsReducer }),
+    EffectsModule.forRoot([DealsEffects]),
   ],
 })
 export class AppStoreModule { }
