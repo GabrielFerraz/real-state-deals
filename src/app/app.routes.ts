4e96d106159fad1bf833e3a6489f42894ef2575e
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2026-07-13 21:55:47 -0300

    redirecting if already logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
 import { DealsComponent } from './features/deals/deals.component';
 import { authGuard } from './guards/auth.guard';
+import { loggedInGuard } from './guards/logged-in.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: AuthComponent },
+    { path: 'login', component: AuthComponent, canActivate: [loggedInGuard] },
     { path: 'deals', component: DealsComponent, canActivate: [authGuard] },
 ];
